3224227c05afb3fdeae0d2dc8aa95f4ecd5e6159
Witold Mescheryakov <witcold@users.noreply.github.com>
2013-12-01 03:45:45 -0800

    Update 3_6.sh
    Добавлено полное решение задачи

--- 3_6.sh
+++ 3_6.sh
@@ -3,4 +3,12 @@
 # 6. Используя /var/log/messages, вывести список IP-адресов, полученных за всё
 # время через DHCP, список отсортированный, уникальный. Для каждого адреса
 # вывести первое и последнее время, когда он был получен.
-grep 'dhcp' messages | grep 'leased' | sed "s/  / /" | cut -d " " -f 1-3,8 | sort -k 4
+ips=`grep 'dhcp' messages | grep 'leased' | sed "s/ / /" | cut -d " " -f 1-3,8 | tee 3.tmp | sed "s/^.*:[0-9][0-9]//" | sort | uniq`;
+for ip in $ips
+do
+  echo IP : $ip;
+  f=`grep "$ip" 3.tmp | head -n 1 | sed "s/$ip//"`;
+  l=`grep "$ip" 3.tmp | tail -n 1 | sed "s/$ip//"`;
+  echo First : $f;
+  echo Last : $l;
+done
